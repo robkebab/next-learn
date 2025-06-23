a47badb910e04d3845eb84fef04b5af37f76708d
Robert Collins <58490986+robkebab@users.noreply.github.com>
2025-06-22 23:07:07 -0500

    add not found error

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -24,10 +24,15 @@ export async function createInvoice(formData: FormData) {
   });
 
   const date = new Date().toISOString().split("T")[0];
-  await sql`
+
+  try {
+    await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amount}, ${status}, ${date})
   `;
+  } catch (error) {
+    console.error(error);
+  }
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
@@ -44,18 +49,21 @@ export async function updateInvoice(id: string, formData: FormData) {
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
-
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amount}, status = ${status}
-    WHERE id = ${id}
-  `;
+  try {
+    await sql`
+  UPDATE invoices
+  SET customer_id = ${customerId}, amount = ${amount}, status = ${status}
+  WHERE id = ${id}
+`;
+  } catch (error) {
+    console.error(error);
+  }
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
 
-export async function deleteInvoice(id: string) {
+export async function deleteInvoice(id: string) {  
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath("/dashboard/invoices");
 }
